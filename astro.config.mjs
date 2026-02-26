c02b381bce8e312abc3dfcc21a0f3de3e453d3ab
zErick93op <ericklarreacarlos@outlook.com>
2026-02-25 21:00:03 -0500

    setup inicial de sitemap (site sin definir)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+ // Como aún no se tiene dominio, puedo usar el localhost solo para probar en local y ver si se genero
+ // Lo dejare vacio hasta tener el dominio y luego ahi hacer un push ya con el dominio 
+ //site: 'debo poner el dominio aqui',
+ integrations: [
+    sitemap()
+ ]
+});
+
+
