cf4112331ec4ce0a92da6a03758732d15ba00966
zErick93op <ericklarreacarlos@outlook.com>
2026-03-10 02:00:13 -0500

    Mejora Diseño y Agregamos Informacion De WSP y Reds Sociales

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
- // Como aún no se tiene dominio, puedo usar el localhost solo para probar en local y ver si se genero
- // Lo dejare vacio hasta tener el dominio y luego ahi hacer un push ya con el dominio 
- //site: 'debo poner el dominio aqui',
+ site: 'https://igc.edu.pe',
  integrations: [
     sitemap()
  ]
